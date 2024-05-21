be8dd4e14a34641204ecb3033a80688996e7d9f6
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-05-21 21:37:46 +0530

    auth added

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 
 const app = express();
-app.use(express.json); 
+app.use(express.json());
 
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
