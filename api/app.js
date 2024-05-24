ac1f0a0590846365c275672b5b2c6e2ef833290a
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-05-24 16:51:51 +0530

    added chat but not working]

--- api/app.js
+++ api/app.js
@@ -1,32 +1,26 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import postRoute from './routes/post.route.js';
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
-import testRoute from './routes/test.route.js';
-
-dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
-
-app.use("/api/posts", postRoute);
-app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server is running at 5000");
 });
