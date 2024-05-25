c84033b411617c185aa4014d1457d2d17e7d10c5
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-05-25 16:42:06 +0530

    ........

--- api/routes/chat.route.js
+++ api/routes/chat.route.js
@@ -10,10 +10,16 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getChats);
-router.get("/:id", verifyToken, getChat);
-router.post("/", verifyToken, addChat);
-router.put("/read/:id", verifyToken, readChat);
+router.get("/", getChats);
+router.get("/:id", getChat);
+router.post("/", addChat);
+router.put("/read/:id", readChat);
 router.get('/:senderId/:receiverId', getChatsBetweenUsers);
+
+// router.get("/", verifyToken, getChats);
+// router.get("/:id", verifyToken, getChat);
+// router.post("/", verifyToken, addChat);
+// router.put("/read/:id", verifyToken, readChat);
+// router.get('/:senderId/:receiverId', getChatsBetweenUsers);
 
 export default router;
