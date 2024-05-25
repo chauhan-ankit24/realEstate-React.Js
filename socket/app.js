611a6a674ed17f64a9bcf4361c4d6c96d27733df
Ankit Chauhan <91563712+chauhan-ankit24@users.noreply.github.com>
2024-05-25 15:09:33 +0530

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: 'https://real-estate-react-js-eight.vercel.app',
+    origin: '*',
   },
 });
 let onlineUser = [];
