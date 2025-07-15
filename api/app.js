07d087417201587c605a0950bd3cdfe2bbc08014
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-07-16 00:33:34 +0530

    Update CORS origin to correct local development URL for improved access

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173/",
       "https://real-estate-react-js-eight.vercel.app/",
     ],
     credentials: true,
